034622bad61ef1e8fb654cff1ca93b000ee98d76
Seun Bond <sadetoye@gmail.com>
2021-01-25 19:00:55 +0100

    AddUser and Meeting (classes) Entities added

---
+++ src/main/java/se/lexicon/samuel/model/AppUser.java
@@ -0,0 +1,98 @@
+package se.lexicon.samuel.model;
+
+import java.util.Collections;
+import java.util.Objects;
+import java.util.TreeSet;
+import java.util.UUID;
+
+public class AppUser {
+    private String userId;
+    private String userName;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private TreeSet<Meeting> meetings; //this will act as a stub
+
+    //creating a constructor for the fields
+    public AppUser(String userId, String userName, String password, String firstName, String lastName, TreeSet<Meeting> meetings) {
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.meetings = meetings;
+    }
+    //creating another constructor again: chaining constructors. these would be the needed parameters to be entered
+    //top constructor will be called from here
+    public AppUser(String userName, String password, String firstName, String lastName) {
+        this(UUID.randomUUID().toString(), userName, password, firstName, lastName, new TreeSet<>());
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public TreeSet<Meeting> getMeetings() {
+        return meetings;
+    }
+
+    public void setMeetings(TreeSet<Meeting> meetings) {
+        this.meetings = meetings;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AppUser)) return false;
+        AppUser appUser = (AppUser) o;
+        return Objects.equals(userId, appUser.userId) &&
+                Objects.equals(userName, appUser.userName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, userName);
+    }
+
+    @Override
+    public String toString() {
+        return "AppUser{" +
+                "userId='" + userId + '\'' +
+                ", userName='" + userName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/se/lexicon/samuel/model/Meeting.java
@@ -0,0 +1,114 @@
+package se.lexicon.samuel.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+public class Meeting implements Comparable<Meeting> {
+    private String meetingId;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String topic;
+    private String agenda;
+    private AppUser organizer;
+    private List<AppUser> participants;
+
+    public Meeting(String meetingId, LocalDateTime start, LocalDateTime end, String topic, String agenda, AppUser organizer, List<AppUser> participants) {
+        this.meetingId = meetingId;
+        this.start = start;
+        this.end = end;
+        this.topic = topic;
+        this.agenda = agenda;
+        this.organizer = organizer;
+        this.participants = participants;
+    }
+    //chain the constructor again to call the above constructor
+    public Meeting(LocalDateTime start, LocalDateTime end, String topic, String agenda, AppUser organizer) {
+        this(UUID.randomUUID().toString(), start, end, topic, agenda, organizer, new ArrayList<>());
+    }
+
+    public String getMeetingId() {
+        return meetingId;
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDateTime start) {
+        this.start = start;
+    }
+
+    public LocalDateTime getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDateTime end) {
+        this.end = end;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getAgenda() {
+        return agenda;
+    }
+
+    public void setAgenda(String agenda) {
+        this.agenda = agenda;
+    }
+
+    public AppUser getOrganizer() {
+        return organizer;
+    }
+
+    public void setOrganizer(AppUser organizer) {
+        this.organizer = organizer;
+    }
+
+    public List<AppUser> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<AppUser> participants) {
+        this.participants = participants;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Meeting)) return false;
+        Meeting meeting = (Meeting) o;
+        return Objects.equals(meetingId, meeting.meetingId) &&
+                Objects.equals(start, meeting.start) &&
+                Objects.equals(end, meeting.end);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(meetingId, start, end);
+    }
+
+    @Override
+    public String  toString() {
+        return "Meeting{" +
+                "meetingId='" + meetingId + '\'' +
+                ", start=" + start +
+                ", end=" + end +
+                ", topic='" + topic + '\'' +
+                ", agenda='" + agenda + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Meeting o) {
+        return getStart().compareTo(o.getStart());
+    }
+}
